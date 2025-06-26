05e8b0ff6cd305e057d9ad08cf9692c1dd8882c3
Egor <example@gmail.com>
2025-06-26 17:38:09 +0700

    add script file, object

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const a = 'Hello';
 const b = 'World';
 
-const c = a + b;
+const c = a + " " + b;
 console.log(c);
+
+const test = {
+    1: 'Test1',
+    2: 'Test2',
+    3: 'Test3'
+}
+
+console.log(test);
+console.log(typeof test);
